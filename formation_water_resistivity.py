5b4cdf353b0c75f506758d7fcb5b76dd5447a66e
vqR` <123908201+vugaer@users.noreply.github.com>
2025-11-05 20:17:42 +0400

    Update formation_water_resistivity.py

--- formation_water_resistivity.py
+++ formation_water_resistivity.py
@@ -1,3 +1,5 @@
+import math
+
 sp_log_reading = float(input('SP(mV): '))
 reservoir_temp = float(input('T(F): '))
 r_mf_x, r_mf_x_temp = map(float, input('R_mf(Ohm.m) @ T(F): ').split('@'))
@@ -14,9 +16,12 @@ if r_mf_reservoir <= 0.1:
 
 r_mfe_reservoir = r_convert_temp(r_mfe, 75, 150)
 
-k = 61 + 0.133 * reservoir_temp # at 150F
 ssp = sp_log_reading
-r_we = r_mfe_reservoir * 10 ** (ssp / k)
+# For NaCl
+r_we = r_mfe_reservoir * 10 ** (ssp / (61 + 0.133 * reservoir_temp))
+
+# for KCl
+# r_we = r_mfe_reservoir * 10 ** (-(22 - ssp) / (56 + 0.12 * reservoir_temp))
 
 r_we_75 = r_convert_temp(r_we, 150, 75)
 
@@ -26,6 +31,8 @@ if r_we_75 <= 0.12:
   r_w_75 = (77*r_we_75+5)/(146-377*r_we_75)
 
 r_w_reservoir = r_convert_temp(r_w_75, 75, 150)
+salinity = 10 ** ((3.562 - math.log10(r_w_75 - 0.0123)) / 0.955)
+
 
 print("\n=== Resistivity and Temperature Summary ===\n")
 print(f"Reservoir Temperature : {reservoir_temp}°F\n")
@@ -37,3 +44,4 @@ print(f"r_we               : {r_we:.3f} Ω·m   | Effective water resistivity at
 print(f"r_we_75            : {r_we_75:.3f} Ω·m   | Effective water resistivity at 75°F")
 print(f"r_w_75             : {r_w_75:.3f} Ω·m   | Water resistivity at 75°F")
 print(f"r_w_reservoir      : {r_w_reservoir:.3f} Ω·m   | Water resistivity at reservoir temperature ({reservoir_temp}°F)")
+print(f"Salinity           : {salinity:.3f} ppm   | Salinity")
